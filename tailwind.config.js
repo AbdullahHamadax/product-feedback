4bba49e52c3ec3130881dede10c43bf0a632f426
AbdullahHamadax <abdallahamada114@gmail.com>
2024-06-26 17:29:23 +0300

    Suggestions,Sidebar done on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "background-color": "var(--background-color)",
-        "sidebar-color": "var(--sidebar-color)",
+        background_body: "var(--background-body)",
+        suggestions_bar: "var(--suggestions-bar)",
+        feedback_title: "var(--feedback-title)",
+        feedback_tag: "var(--feedback-tag)",
+        feedback_paragraph: "var(--feedback-paragraph)",
+        feedback_upvote: "var(--feedback-upvote)",
+        feedback_tag_card_hover: "var(--feedback-tag-card-hover)",
+        add_feedback_button: "var(--add-feedback-button)",
+        add_feedback_button_hover: "var(--add-feedback-button-hover)",
+        header_faded: "var(--header-faded)",
+        list_orange: "var(--list-orange)",
+        list_cyan: "var(--list-cyan)",
       },
       fontFamily: {
-        josta: "Josta",
+        jost: ["Jost"],
       },
     },
   },
